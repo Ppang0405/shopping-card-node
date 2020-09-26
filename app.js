76450e641c402b49f61f348b42b0fbca812ff3f6
Truong Nguyen <nguyentruong.xyz0405@gmail.com>
2020-09-26 10:51:17 +0700

    set up port 3000

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,6 +36,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(3000, () => {
+    console.log(
+        "The server has started on port 3000. Head to localhost:3000 in the browser and see what's there!"
+    );
 });
 
 module.exports = app;
